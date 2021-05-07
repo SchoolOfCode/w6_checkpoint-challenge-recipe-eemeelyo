946618c258c13c4e04ff2dd52456cd947326899d
EmilioBC <62893436+EmilioBC@users.noreply.github.com>
2021-05-07 19:13:23 +0100

    Started the recipe

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const YOUR_APP_ID = "2a5d65c7";
+const YOUR_APP_KEY = "b94095accb136e0638ba880341dd3584";
 let foodToSearch = null;
 
 function handleRecipeClick() {
@@ -9,6 +11,12 @@ function handleFoodChange() {
 }
 
 async function fetchRecipe(food) {
+  const requestUrl = await fetch(
+    `https://api.edamam.com/search?q=kale&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`
+  );
+  const dataResponse = await requestUrl.json();
+  console.log(dataResponse);
+
   //--- write your code below ---
   //--- write your code above ---
 }
