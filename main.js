06e742b2736652fa7683916ba2ebe05017b7a218
EmilioBC <62893436+EmilioBC@users.noreply.github.com>
2021-05-14 21:23:05 +0100

    Sorting out the designs and API

--- main.js
+++ main.js
@@ -16,26 +16,78 @@ async function fetchRecipe(food) {
   );
   const dataResponse = await requestUrl.json();
   const recipes = dataResponse.hits;
-  const firstRecipe = recipes[0].recipe;
-  console.log(firstRecipe);
-  display(firstRecipe);
-  // return dataResponse;
 
+  const firstRecipe = recipes[0].recipe;
+  const secondRecipe = recipes[1].recipe;
+  const thirdRecipe = recipes[2].recipe;
+  // console.log(firstRecipe);
   //--- write your code below ---
   //--- write your code above ---
+  display(firstRecipe);
+  display(secondRecipe);
+  display(thirdRecipe);
+  console.log(recipes);
 }
 
-const displayImage = document.getElementById("recipeImg");
-const linkTag = document.getElementById("recipe-label");
+const displayImage1 = document.getElementById("recipeImg1");
+const linkTag1 = document.getElementById("recipe-label1");
+const displayImage2 = document.getElementById("recipeImg2");
+const linkTag2 = document.getElementById("recipe-label2");
+const displayImage3 = document.getElementById("recipeImg3");
+const linkTag3 = document.getElementById("recipe-label3");
 
 function display(firstRecipe) {
   const label = firstRecipe.label;
-  const recipeImage = firstRecipe.image;
-  const recipeLink = firstRecipe.url;
+  const recipeImage1 = firstRecipe.image;
+  const recipeLink1 = firstRecipe.url;
+  displayImage1.src = recipeImage1;
+  linkTag1.href = recipeLink1;
+  linkTag1.innerText = label;
+}
+function display(secondRecipe) {
+  const label = secondRecipe.label;
+  const recipeImage2 = secondRecipe.image;
+  const recipeLink2 = secondRecipe.url;
+  displayImage2.src = recipeImage2;
+  linkTag2.href = recipeLink2;
+  linkTag2.innerText = label;
+}
+function display(firstRecipe) {
+  const label = firstRecipe.label;
+  const recipeImage1 = firstRecipe.image;
+  const recipeLink1 = firstRecipe.url;
+  displayImage1.src = recipeImage1;
+  linkTag1.href = recipeLink1;
+  linkTag1.innerText = label;
+}
 
-  displayImage.src = recipeImage;
-  linkTag.href = recipeLink;
-  linkTag.innerText = label;
+// async function apiBG() {
+//   const backG = await fetch(
+//     "https://source.unsplash.com/1000x1200/?nature,black"
+//   );
+//   const scenery = await backG.json();
+//   return scenery;
+//   console.log(scenery);
+// }
+
+// function randomBG() {
+//   document.body.style.background = apiBG();
+// }
+// randomBG();
+
+function randomBG() {
+  document.body.style.background =
+    "#f3f3f3 url('https://source.unsplash.com/1000x1200/?nature,black') no-repeat center center/cover";
+}
+randomBG();
+
+// async function backgroundImage() {
+//   const
+// }
+const rightButton = document.getElementById("right");
+
+function forwardRecipes() {
+  for (let i = 0; i < recipes.length; i++) {}
 }
 
 // Get the name
